8be657e52767a1910171636ffc7391a4ca1594db
brunoelinton <brunoelinton@outlook.com>
2022-03-28 17:18:36 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,22 +1,40 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 // REST CONTROLLER
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	// TESTING USER RESOURCE, FROM GET REQUISITION IN HTTP
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+	public ResponseEntity<List<User>> findAll(){
+		
+		// END POINT IMPLEMENTATION FOR GET ALL USERS
+		List<User> list = service.findAll();
 		// RETURN A GOOD RESPONSE TO REQUISITON HTTP FROM PAGE BODY
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(list);
 	}
+	
+	// TESTING USER RESOURCE, FROM GET REQUISITION IN HTTP
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
